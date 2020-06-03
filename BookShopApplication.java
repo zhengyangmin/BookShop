beedef6bce08be6932933d829def34390e1561dc
闵政扬 <50236407+zhengyangmin@users.noreply.github.com>
2020-06-03 17:21:39 +0800

    Add files via upload

---
+++ BookShopApplication.java
@@ -0,0 +1,14 @@
+package com.book.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopApplication.class, args);
+        System.out.println("http://localhost:9000/book");
+    }
+
+}
